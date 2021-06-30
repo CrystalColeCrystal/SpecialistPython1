eee266e3a869f69c8e732f9841bda47d22663f4e
CrystalColeCrystal <86655027+CrystalColeCrystal@users.noreply.github.com>
2021-06-30 11:52:13 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -5,3 +5,10 @@ text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
 # TODO: your code here
+text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
+text = text.split(" ")
+count = 0
+for i in range (0, len(text)):
+    if len(text[i])>5:
+        count+=1
+print(count)
