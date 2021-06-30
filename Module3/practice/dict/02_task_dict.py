bd4f6931cd35d076f111bf7adbbf675e24a61010
CrystalColeCrystal <86655027+CrystalColeCrystal@users.noreply.github.com>
2021-06-30 17:09:50 +0300

    Update 02_task_dict.py

--- Module3/practice/dict/02_task_dict.py
+++ Module3/practice/dict/02_task_dict.py
@@ -1,9 +1,14 @@
 # Даны два списка одинаковой длины. Необходимо создать из них словарь таким образом,
 # чтобы элементы первого списка были ключами, а элементы второго — соответственно значениями нашего словаря.
 
-keys = ['name', 'surname', 'age', 'rate']
-values = ['Петр', 'Первый', 42, 1300]
 
 # TODO: your code here
 # Нужно получить словарь:
 # {'name': 'Петр', 'surname': 'Первый', 'age': 42, 'rate': 1300}
+
+keys = ['name', 'surname', 'age', 'rate']
+values = ['Петр', 'Первый', 42, 1300]
+dictionary = dict()
+for key, value in zip(keys, values):
+    dictionary[key] = value
+print(dictionary)
