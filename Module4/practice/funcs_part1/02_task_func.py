50c4eea514926276e0251d461a225c8f01f583b4
CrystalColeCrystal <86655027+CrystalColeCrystal@users.noreply.github.com>
2021-07-01 12:30:15 +0300

    Update 02_task_func.py
    Решения без перевода к строке

--- Module4/practice/funcs_part1/02_task_func.py
+++ Module4/practice/funcs_part1/02_task_func.py
@@ -4,10 +4,15 @@
 # * попробуйте решить данную задачу, не преобразуя число к строке
 
 def palindrome(number):
-    pass
+    lst=[]
+    while number>0:
+        lst.append(number%10)
+        number=number//10
+    for i in range(0, int(len(lst)/2)):
+        if lst[i]!=lst[len(lst)-i-1]:
+            return "NO"
+    return "YES"
 
-
-# Тестируем функцию
 print(palindrome(3454))
 print(palindrome(3443))
 print(palindrome(1234541))
